e3a7cc5171af1db04ac6eb1a113380c6ab469f69
riesposi <riccardo.esposito@lnf.infn.it>
2026-02-18 14:16:08 +0000

    Ready for ibek

--- iocBoot/streamdeviceSample/st.cmd
+++ iocBoot/streamdeviceSample/st.cmd
@@ -22,45 +22,10 @@ drvAsynIPPortConfigure("TTI1", "192.168.197.125:4001", 0, 0, 0)
 
 asynSetOption("TTI1", 0, "disconnectOnReadTimeout", "Y")
 
-dbLoadRecords("db/TTI.template", "P=LAB:,R=PS1:,PORT=TTI1")
+dbLoadRecords("db/TTI.template", "P=SPARC:,R=TTI:,PORT=TTI1")
 
 cd "$(TOP)/iocBoot/$(IOC)"
 
 iocInit
 epicsThreadSleep 1
-
-
-
-
-
-
-
-# epicsEnvSet("IOC","streamdeviceSample")
-# epicsEnvSet("TOP","/app/TTI-magnets-")
-# epicsEnvSet("EPICS_BASE","/epics/epics-base")
-# epicsEnvSet("ASYN","/epics/support/asyn")
-# epicsEnvSet("CALC","/epics/support/calc")
-# epicsEnvSet("SUPPORT","/epics/support")
-# epicsEnvSet("STREAMDEVICE","/epics/support/StreamDevice")
-# epicsEnvSet("MODBUS","/epics/support/modbus")
-# epicsEnvSet("STREAM_PROTOCOL_PATH", "$(TOP)/db")
-
-# cd "${TOP}"
-
-# dbLoadDatabase "dbd/streamdeviceSample.dbd"
-# streamdeviceSample_registerRecordDeviceDriver pdbbase
-
-# drvAsynIPPortConfigure("TTI1", "192.168.197.125:4001", 0, 0, 0)
-
-# # asynSetOption("TTI1", 0, "disconnectOnReadTimeout", "Y")
-
-# # asynSetTraceMask("TTI1", 0, 0x9)
-# # asynSetTraceIOMask("TTI1", 0, 0x2)
-
-# dbLoadRecords("db/TTI.template", "P=LAB:,R=PS1:,PORT=TTI1")
-
-# cd "${TOP}/iocBoot/${IOC}"
-
-# iocInit
-# epicsThreadSleep 1
 dbl 
